cab741a51116512a8b7f369cffa99a3ec07588fe
bh72df <bh72df@student.sunderland.ac.uk>
2023-10-23 21:26:07 +0100

    added speedometer and camera switch

---
+++ Assets/Car/Scripts/Speedometer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour
+{
+    public Text speedText;
+    public Rigidbody carRigidbody;
+
+    void Update()
+    {
+        float speed = carRigidbody.velocity.magnitude * 1f; 
+        speedText.text = " " + speed.ToString("F1") + " mph";
+    }
+}
